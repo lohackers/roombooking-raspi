c842deedbcf6a8c24d2064ac5067f8c1d00c41b9
Matteo Rosati <rosati.matteo@gmail.com>
2013-12-18 19:33:18 +0100

    Stable version for demo ;)

--- roombooking.py
+++ roombooking.py
@@ -18,8 +18,8 @@ upcoming = config.get('DEFAULT', 'upcoming')
 onair = config.get('DEFAULT', 'onair')
 polling = int(config.get('DEFAULT', 'polling'))
 
-os.system("gpio mode 0 out")
 os.system("gpio mode 1 out")
+os.system("gpio mode 7 out")
 
 # Main routine
 def main():
@@ -27,21 +27,32 @@ def main():
 	opener.addheaders = [('Accept', 'application/json')]
 
 	while True:
-		upcoming_booking = len(loads(opener.open(upcoming).read()))
-		onair_booking = len(loads(opener.open(onair).read()))
+		upcoming_booking = loads(opener.open(upcoming).read())
+		onair_booking = loads(opener.open(onair).read())
+		lcd.clear()
 
 		if onair_booking:
 			os.system("gpio write 1 1")
 			os.system("gpio write 7 0")
-			print 'ON AIR NOW!'
+
+			time_from = onair_booking[0]["from"].split(" ")[1].split(":")
+			time_to = onair_booking[0]["to"].split(" ")[1].split(":")
+
+			lcd.message(time_from[0]+":"+time_from[1] + " - " + time_to[0]+":"+time_to[1] + "\n" + onair_booking[0]["user"]["email"].split("@")[0])
+
 		elif upcoming_booking:
 			os.system("gpio write 1 0")
 			os.system("gpio write 7 1")
-			print 'EVENT IS COMING SOON'
+
+			time_from = upcoming_booking[0]["from"].split(" ")[1].split(":")
+			time_to = upcoming_booking[0]["to"].split(" ")[1].split(":")
+
+			lcd.message(time_from[0]+":"+time_from[1] + " - " + time_to[0]+":"+time_to[1] + "\n" + upcoming_booking[0]["user"]["email"].split("@")[0])
+		
 		else:
 			os.system("gpio write 1 0")
 			os.system("gpio write 7 0")
-			print 'NO EVENTS!'
+			lcd.message("NO EVENTS!")
 
 		sleep(polling)
 
